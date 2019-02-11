36ba08d1841b57f31f2741991f71712b343c544c
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2019-02-11 09:30:06 +0700

    Showed Multiplication Table

--- multiply/views.py
+++ multiply/views.py
@@ -3,11 +3,15 @@ from django.template import loader
 
 def mulitiplication(request):
     if request.POST.get('num'):
-        number = request.POST.get('num')
+        number = int(request.POST.get('num'))
         results = []
-        for r in range(int(number)):
-            results.append(number)
-        return render(request,'multiply/multiTable.html',{'results' : results})
+        for i in range(1,13):
+            if(i==1):
+                 results.append(number)
+            else:
+                 result = results[i-2]+number
+                 results.append(result)
+        return render(request,'multiply/multiTable.html',{'results' : results,'number':number,})
     else:
         return render(request,'multiply/index.html',{'error_message': "You didn't insert input."})
 
