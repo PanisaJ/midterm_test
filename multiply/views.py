b9d8b08a53bb294c65ff22fbf218fe8afed527ef
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2019-02-11 09:05:31 +0700

    used loop to show number depend on inputted number

--- multiply/views.py
+++ multiply/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import HttpResponse, render
 from django.template import loader
 
 def mulitiplication(request):
-    if request.POST.get('number'):
-        number = request.POST.get('number')
-        a = ""
-        for r in range(number):
-            a = a + " " + str(number)
-        return HttpResponse(a)
+    if request.POST.get('num'):
+        number = request.POST.get('num')
+        results = []
+        for r in range(int(number)):
+            results.append(number)
+        return render(request,'multiply/multiTable.html',{'results' : results})
+    else:
+        return render(request,'multiply/index.html',{'error_message': "You didn't insert input."})
 
 def index(request):
     return render(request,'multiply/index.html')
